c36fbc0b08a20f4b6b16689f3f06f362de491a15
Durvesh Danve <58652202+DurvAcee@users.noreply.github.com>
2021-07-12 12:18:45 +0530

    Update settings.py

--- Internet Service Provider/demoProj/settings.py
+++ Internet Service Provider/demoProj/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@r%$jzpjs0axet&4dd)v2xsljo_98o+tw@3l=w@d(*446f9_gt'
+SECRET_KEY = 'YOUR_SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
